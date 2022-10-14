dff096eba3be3f4fc150eff8aa64c84564e7365f
mittal <gohilmittal144@gmail.com>
2022-10-14 09:33:35 +0530

    Js Assignment

---
+++ script.js
@@ -0,0 +1,50 @@
+// document.getElementById("m").innerHTML = ("Gohil");
+// alert(9 + 10);
+// console.log("Hi.....");
+// console.log("mitz");
+// document.write("Mitz Rajput");
+
+//------------Create Variable--------------
+/*var m;
+m = 5;
+alert(m); */
+
+//------------create string varible-----------
+// var mitz;
+// mitz = "my name";
+// alert(mitz);
+
+//--------multiple variable store in onther var-----------
+/* var b = 5;
+var m = 40;
+var z = b + m;
+console.log(z);
+alert(z); */
+//----variable start with $ _ And letter Not Strart With Numbers------------
+//---------Case Sensitive-------
+//-------Es-6 New Ways To Define Variable----------
+// let m = 5;
+// var j = 6;
+// let mitz = m + j;
+// console.log(mitz);
+//-----Change Value Of Any Variables-----------
+// let _k = 'hi';
+// let z = 40;
+// _k = 'mitz';
+// console.log(_k)
+//--------Const Unchange value----------
+
+// const $p = 42;
+// // p = 85;
+// console.log($p) //not Work
+
+//----------------multiple var create one line-------
+// let m = 5,
+//     y = 'mitzz';
+// console.log(y);
+// let d; //declare
+// // d = 5; //assign
+// console.log(d);
+
+
+//
